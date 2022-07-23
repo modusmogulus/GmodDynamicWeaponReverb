2a65dca8e0333ed844318a7eba010bd124544652
relaxtakenotes <91839650+relaxtakenotes@users.noreply.github.com>
2022-07-23 13:07:18 +0500

    untested code
    - added a script to update the precache lua easily
    - replaced the placeholders with actual audio files (they're all the same though)
    - made the base for the mod

--- dwrV3/lua/autorun/client/dwr_client.lua
+++ dwrV3/lua/autorun/client/dwr_client.lua
@@ -1,4 +1,16 @@
--- Receive entityfire_networked and do all the calculations.
 -- Don't forget about the path shit: (AMMOTYPE/INDOORS||OUTDOORS/CLOSE||DISTANT)
 
 print("[DWRV3] Client loaded.")
+
+if !dwr_successfullyCached then return end
+
+net.Receive("EntityFireBullets_networked", function()
+	local attacker = net.ReadEntity()
+	local data = net.ReadTable()
+	-- do whatever
+end)
+
+net.Receive("EntityEmitSound_networked", function() 
+	local data = net.ReadTable()
+	-- do whatever
+end)
--- dwrV3/lua/autorun/server/dwr_server.lua
+++ dwrV3/lua/autorun/server/dwr_server.lua
@@ -1,3 +1,21 @@
--- EntityFireBullets hook: network all of its parameters to all players and then optimize out the ones we dont use.
+-- network only the data we use before we release it on workshop (or don't, it wont really matter, i just like optimization - jp4)
 
 print("[DWRV3] Server loaded.")
+
+util.AddNetworkString("EntityFireBullets_networked")
+util.AddNetworkString("EntityEmitSound_networked")
+
+if !dwr_successfullyCached then return end
+
+hook.Add("EntityFireBullets", "dwr_EntityFireBullets", function(ent, data)
+    net.Start("EntityFireBullets_networked")
+    net.WriteEntity(ent)
+    net.WriteTable(data)
+    net.Broadcast()
+end)
+
+hook.Add("EntityEmitSound", "dwr_EntityEmitSound", function(data) 
+    net.Start("EntityEmitSound_networked")
+    net.WriteTable(data)
+    net.Broadcast()
+end)
