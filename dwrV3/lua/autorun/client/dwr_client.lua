6317853cb7f53ad7a1ef4c1358ee27cf28dc0bcd
relaxtakenotes <91839650+relaxtakenotes@users.noreply.github.com>
2022-07-25 11:47:23 +0500

    slight changes
    more pretty

--- dwrV3/lua/autorun/client/dwr_client.lua
+++ dwrV3/lua/autorun/client/dwr_client.lua
@@ -1,10 +1,8 @@
 print("[DWRV3] Client loaded.")
 
-
-
 local function traceableToSky(pos, offset)
     local tr = util.TraceLine({start=pos + offset, endpos=pos + Vector(offset.x, offset.y, 100000000), mask=MASK_NPCWORLDSTATIC})
-	local temp = util.TraceLine({start=tr.StartPos, endpos=pos, mask=MASK_NPCWORLDSTATIC})
+	local temp = util.TraceLine({start=tr.StartPos, endpos=pos, mask=MASK_NPCWORLDSTATIC}) -- doing this because sometimes the trace can go oob and even rarely there are cases where i cant see if it spawned oob
 
     if temp.HitPos == pos and tr.HitSky then
     	return true
@@ -60,7 +58,6 @@ local function getEntriesStartingWith(pattern, array)
 end
 
 local function playReverb(reverbSoundFile, positionState, distanceState, dataSrc, customVolumeMultiplier)
-
 	if GetConVar("sv_dwr_disable_reverb"):GetBool() == true then return end
 	local earpos = LocalPlayer():GetViewEntity():GetPos()
 
@@ -92,16 +89,13 @@ local function playReverb(reverbSoundFile, positionState, distanceState, dataSrc
 		local distance = earpos:Distance(dataSrc) * 0.01905 -- in meters
 		local distanceMultiplier = 500/distance^2
 		volume = math.Clamp(volume * distanceMultiplier, 0, 1)
-		print(distance)
+		print("[DWR] Distance (Meters): " .. distance)
 	end
 
-	local delayBySoundSpeed
+	local delayBySoundSpeed = 0
 	if GetConVar("sv_dwr_disable_soundspeed"):GetBool() == false then
 		delayBySoundSpeed = dataSrc:Distance(earpos) * 0.01905 / GetConVar("sv_dwr_soundspeed"):GetInt()
-	else
-		delayBySoundSpeed = 0
 	end
-
 
 	timer.Simple(delayBySoundSpeed, function()
 		EmitSound(reverbSoundFile, LocalPlayer():EyePos(), -2, CHAN_STATIC, volume * (GetConVar("sv_dwr_volume"):GetInt() / 100), soundLevel, soundFlags, pitch, dsp)
@@ -126,11 +120,12 @@ net.Receive("dwr_EntityFireBullets_networked", function(len)
 
 	print("[DWR] dwr_EntityFireBullets_networked received")
 
-	if GetConVar("sv_dwr_disable_indoors_reverb"):GetBool() == true && getPositionState(dataSrc) == "indoors" then return end
-	if GetConVar("sv_dwr_disable_outdoors_reverb"):GetBool() == true && getPositionState(dataSrc) == "outdoors" then return end
-
 	-- looking for reverb soundfiles to use
 	local positionState = getPositionState(dataSrc)
+
+	if GetConVar("sv_dwr_disable_indoors_reverb"):GetBool() == true && positionState == "indoors" then return end
+	if GetConVar("sv_dwr_disable_outdoors_reverb"):GetBool() == true && positionState == "outdoors" then return end
+
 	local distanceState = getDistanceState(dataSrc, earpos)
 	local ammoType = formatAmmoType(dataAmmoType)
 	local reverbOptions = getEntriesStartingWith("dwr" .. "/" .. ammoType .. "/" .. positionState .. "/" .. distanceState .. "/", dwr_reverbFiles)
@@ -142,13 +137,10 @@ net.Receive("dwr_EntityFireBullets_networked", function(len)
 end)
 
 hook.Add("EntityEmitSound", "dwr_EntityEmitSound", function(data)
-
-	
-
 	local earpos = LocalPlayer():GetViewEntity():GetPos()
 
 	if not string.find(data.SoundName, "explo") then return end
-	print("[DWR] EntityEmitSound")
+	print("[DWR] EntityEmitSound (Explosion)")
 
 	-- looking for reverb soundfiles to use
 	local positionState = getPositionState(data.Pos)
