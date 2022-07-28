a43c7b6af32f1130bf78bfe30f3ab3d1da755226
relaxtakenotes <91839650+relaxtakenotes@users.noreply.github.com>
2022-07-28 20:21:30 +0500

    i should take a break LOL

--- dwrV3/lua/autorun/client/dwr_client.lua
+++ dwrV3/lua/autorun/client/dwr_client.lua
@@ -58,7 +58,7 @@ local function getEntriesStartingWith(pattern, array)
 			table.insert(tempArray, path)
 		end
 	end
-	if true then --table.IsEmpty(tempArray) then
+	if table.IsEmpty(tempArray) then
 		print("[DWR] WTF. Nothing found??? Here's debug info!!!", pattern, table.ToString(dwr_reverbFiles, "debug", false))
 		return {"dwr/kleiner.wav"}
 	end
