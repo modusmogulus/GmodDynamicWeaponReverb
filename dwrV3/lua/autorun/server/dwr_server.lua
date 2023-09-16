2de83f2fba41e8ceea7dabdf5137f359439fa42c
relaxtakenotes <91839650+relaxtakenotes@users.noreply.github.com>
2023-09-16 11:41:37 +0500

    sync with the workshop version

--- dwrV3/lua/autorun/server/dwr_server.lua
+++ dwrV3/lua/autorun/server/dwr_server.lua
@@ -1,12 +1,11 @@
 print("[DWRV3] Server loaded.")
+
 util.AddNetworkString("dwr_EntityFireBullets_networked")
 util.AddNetworkString("dwr_EntityEmitSound_networked")
 util.AddNetworkString("dwr_sync_blacklist")
 
 local networkSoundsConvar = CreateConVar("sv_dwr_network_sounds", "0", {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Network server-only gunshots to clients in order for them to get processed as well. Introduces delay to weapon firing.")
-
 local networkGunshotsConvar = CreateConVar("sv_dwr_network_reverb_pas", "0", {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Network gunshot events only to people that are considered in range by the game.")
-
 local blacklist = {}
 
 if not file.Read("dwr_sv_weapon_blacklist.json") or #file.Read("dwr_sv_weapon_blacklist.json") == 0 then
@@ -21,7 +20,7 @@ local function changeBlacklist(action, weaponClass)
     local JSONData = file.Read("dwr_sv_weapon_blacklist.json")
     local converted = util.JSONToTable(JSONData) or {}
 
-    if action == "remove" then
+    if action == "remove" then 
         print("Removed " .. weaponClass .. " from the blacklist.")
         converted[weaponClass] = nil
     end
@@ -31,51 +30,39 @@ local function changeBlacklist(action, weaponClass)
         converted[weaponClass] = true
     end
 
-    if action == "clear" then
+    if action == "clear" then 
         print("Blacklist cleared.")
         converted = {}
     end
 
     blacklist = converted
     file.Write("dwr_sv_weapon_blacklist.json", util.TableToJSON(blacklist))
+
     net.Start("dwr_sync_blacklist")
-    net.WriteTable(blacklist)
+        net.WriteTable(blacklist)
     net.Broadcast()
 end
 
 local function removeWeaponFromBlacklist(ply, cmd, args)
-    if not args[1] then
-        print("Missing weapon class.")
-
-        return
-    end
-
+    if not args[1] then print("Missing weapon class.") return end
     changeBlacklist("remove", args[1])
 end
-
 concommand.Add("sv_dwr_blacklist_remove", removeWeaponFromBlacklist, nil, "Remove your current weapon from the blacklist.")
 
 local function addWeaponToBlacklist(ply, cmd, args)
-    if not args[1] then
-        print("Missing weapon class.")
-
-        return
-    end
-
+    if not args[1] then print("Missing weapon class.") return end
     changeBlacklist("add", args[1])
 end
-
 concommand.Add("sv_dwr_blacklist_add", addWeaponToBlacklist, nil, "Blacklist your current weapon from being affected by this mod. (clients will be able to override this)")
 
 local function clearBlacklist(ply, cmd, args)
     changeBlacklist("clear", nil)
 end
-
 concommand.Add("sv_dwr_blacklist_clear", clearBlacklist, nil, "Clear the blacklist from anything and everything.")
 
-hook.Add("PlayerSpawn", "dwr_blacklist_sync", function()
+hook.Add("PlayerSpawn", "dwr_blacklist_sync", function() 
     net.Start("dwr_sync_blacklist")
-    net.WriteTable(blacklist)
+        net.WriteTable(blacklist)
     net.Broadcast()
 end)
 
@@ -86,22 +73,21 @@ local function writeVectorUncompressed(vector)
 end
 
 local function getSuppressed(weapon, weaponClass)
-    if string.StartWith(weaponClass, "arccw_") and weapon:GetBuff_Override("Silencer") then
-        return true
-    elseif string.StartWith(weaponClass, "tfa_") and weapon:GetSilenced() then
-        return true
+    if string.StartWith(weaponClass, "arccw_") and weapon:GetBuff_Override("Silencer") then return true
+    elseif string.StartWith(weaponClass, "tfa_") and weapon:GetSilenced() then return true
     elseif string.StartWith(weaponClass, "mg_") or weaponClass == mg_valpha then
         if not weapon.GetAllAttachmentsInUse then return false end
-
         for slot, attachment in pairs(weapon:GetAllAttachmentsInUse()) do
             if string.find(attachment.ClassName, "silence") or string.find(attachment.ClassName, "suppress") then return true end
         end
     elseif string.StartWith(weaponClass, "cw_") then
-        if weapon.ActiveAttachments ~= nil then
+        if weapon.ActiveAttachments != nil then
             for k, v in pairs(weapon.ActiveAttachments) do
                 if v == false then continue end
                 local att = CustomizableWeaponry.registeredAttachmentsSKey[k]
-                if att.isSuppressor then return true end
+                if att.isSuppressor then
+                    return true
+                end
             end
         end
     end
@@ -111,31 +97,24 @@ end
 
 local function networkGunshotEvent(data)
     net.Start("dwr_EntityFireBullets_networked", false)
-    writeVectorUncompressed(data.Src)
-    writeVectorUncompressed(data.Dir)
-    writeVectorUncompressed(data.Vel) -- velocity
-    writeVectorUncompressed(data.Spread)
-    net.WriteString(data.Ammotype)
-    net.WriteBool(data.isSuppressed)
-    net.WriteEntity(data.Entity) -- to exclude them in MP. they're going to get hook data anyway
-
-    if networkGunshotsConvar:GetBool() then
-        net.SendPAS(data.Src)
-    else
-        net.Broadcast()
-    end
+        writeVectorUncompressed(data.Src)
+        writeVectorUncompressed(data.Dir)
+        writeVectorUncompressed(data.Vel) -- velocity
+        writeVectorUncompressed(data.Spread)
+        net.WriteString(data.Ammotype)
+        net.WriteBool(data.isSuppressed)
+        net.WriteEntity(data.Entity) -- to exclude them in MP. they're going to get hook data anyway
+    if networkGunshotsConvar:GetBool() then net.SendPAS(data.Src) else net.Broadcast() end
 end
 
 function arc9_dwr_detour(args)
     local bullet = args[2]
     local attacker = bullet.Attacker
 
-    if attacker.dwr_shotThisTick == nil then
-        attacker.dwr_shotThisTick = false
-    end
-
+    if attacker.dwr_shotThisTick == nil then attacker.dwr_shotThisTick = false end
     if attacker.dwr_shotThisTick then return end
-    if table.Count(bullet.Damaged) ~= 0 or bullet.dwr_detected then return end
+    if table.Count(bullet.Damaged) != 0 or bullet.dwr_detected then return end
+
     local weapon = bullet.Weapon
     local weaponClass = weapon:GetClass()
     local isSuppressed = getSuppressed(weapon, weaponClass)
@@ -145,39 +124,32 @@ function arc9_dwr_detour(args)
     local vel = bullet.Vel
 
     timer.Simple(0, function()
-        local data = {}
+        data = {}
         data.Src = pos
         data.Dir = dir
         data.Vel = vel
-        data.Spread = Vector(0, 0, 0)
+        data.Spread = Vector(0,0,0)
         data.Ammotype = ammotype
         data.isSuppressed = isSuppressed
         data.Entity = attacker
         data.Weapon = attacker:GetActiveWeapon()
         networkGunshotEvent(data)
     end)
-
     bullet.dwr_detected = true
     attacker.dwr_shotThisTick = true
 
-    timer.Simple(engine.TickInterval() * 2, function()
-        attacker.dwr_shotThisTick = false
-    end)
+    timer.Simple(engine.TickInterval()*2, function() attacker.dwr_shotThisTick = false end)
 end
 
 hook.Add("InitPostEntity", "dwr_create_physbul_hooks", function()
     if ARC9 then
-        -- a = old function
-        function dwr_wrapfunction(a)
-            return function(...)
-                local args = {...}
-
-                arc9_dwr_detour(args)
-
-                return a(...)
-            end
+        function dwr_wrapfunction(a)    -- a = old function
+          return function(...)
+            local args = { ... }
+            arc9_dwr_detour(args)
+            return a(...)
+          end
         end
-
         ARC9.SendBullet = dwr_wrapfunction(ARC9.SendBullet)
     end
 
@@ -186,8 +158,10 @@ hook.Add("InitPostEntity", "dwr_create_physbul_hooks", function()
             local latestPhysBullet = TFA.Ballistics.Bullets["bullet_registry"][table.Count(TFA.Ballistics.Bullets["bullet_registry"])]
             if latestPhysBullet == nil then return end
             if latestPhysBullet["dwr_detected"] then return end
+
             local weapon = latestPhysBullet["inflictor"]
             local weaponClass = weapon:GetClass()
+
             local isSuppressed = getSuppressed(weapon, weaponClass)
             local pos = latestPhysBullet["bul"]["Src"]
             local ammotype = weapon.Primary.Ammo
@@ -195,27 +169,22 @@ hook.Add("InitPostEntity", "dwr_create_physbul_hooks", function()
             local vel = latestPhysBullet["velocity"]
             local entity = latestPhysBullet["inflictor"]:GetOwner()
 
-            if entity.dwr_shotThisTick == nil then
-                entity.dwr_shotThisTick = false
-            end
-
+            if entity.dwr_shotThisTick == nil then entity.dwr_shotThisTick = false end
             if entity.dwr_shotThisTick then return end
             entity.dwr_shotThisTick = true
+            timer.Simple(engine.TickInterval()*2, function() entity.dwr_shotThisTick = false end)
 
-            timer.Simple(engine.TickInterval() * 2, function()
-                entity.dwr_shotThisTick = false
-            end)
-
-            local data = {}
+            data = {}
             data.Src = pos
             data.Dir = dir
             data.Vel = vel
-            data.Spread = Vector(0, 0, 0)
+            data.Spread = Vector(0,0,0)
             data.Ammotype = ammotype
             data.isSuppressed = isSuppressed
             data.Entity = latestPhysBullet["inflictor"]:GetOwner()
             data.Weapon = latestPhysBullet["inflictor"]
             networkGunshotEvent(data)
+
             latestPhysBullet["dwr_detected"] = true
         end)
     end
@@ -228,43 +197,38 @@ hook.Add("InitPostEntity", "dwr_create_physbul_hooks", function()
             if latestPhysBullet["Attacker"] == Entity(0) then return end
             local entity = latestPhysBullet["Attacker"]
 
-            if entity.dwr_shotThisTick == nil then
-                entity.dwr_shotThisTick = false
-            end
-
+            if entity.dwr_shotThisTick == nil then entity.dwr_shotThisTick = false end
             if entity.dwr_shotThisTick then return end
             entity.dwr_shotThisTick = true
-
-            timer.Simple(engine.TickInterval() * 2, function()
-                entity.dwr_shotThisTick = false
-            end)
+            timer.Simple(engine.TickInterval()*2, function() entity.dwr_shotThisTick = false end)
 
             local weapon = latestPhysBullet["Weapon"]
             local weaponClass = weapon:GetClass()
+
             local isSuppressed = getSuppressed(weapon, weaponClass)
             local pos = latestPhysBullet["Pos"]
             local ammotype = weapon.Primary.Ammo
             local dir = latestPhysBullet["Vel"]:Angle():Forward()
             local vel = latestPhysBullet["Vel"]
-            local data = {}
+
+            data = {}
             data.Src = pos
             data.Dir = dir
             data.Vel = vel
-            data.Spread = Vector(0, 0, 0)
+            data.Spread = Vector(0,0,0)
             data.Ammotype = ammotype
             data.isSuppressed = isSuppressed
             data.Entity = latestPhysBullet["Attacker"]
             data.Weapon = latestPhysBullet["Attacker"]:GetActiveWeapon()
             networkGunshotEvent(data)
+            
             latestPhysBullet["dwr_detected"] = true
         end)
     end
 
-    -- global var from mw2019 sweps
-    if MW_ATTS then
+    if MW_ATTS then -- global var from mw2019 sweps
         hook.Add("OnEntityCreated", "dwr_detectmw2019phys", function(ent)
-            if ent:GetClass() ~= "mg_sniper_bullet" and ent:GetClass() ~= "mg_slug" then return end
-
+            if ent:GetClass() != "mg_sniper_bullet" and ent:GetClass() != "mg_slug" then return end
             timer.Simple(0, function()
                 local attacker = ent:GetOwner()
                 local entity = attacker
@@ -274,31 +238,23 @@ hook.Add("InitPostEntity", "dwr_create_physbul_hooks", function()
                 local vel = ent:GetAngles():Forward() * ent.Projectile.Speed
                 local isSuppressed = getSuppressed(weapon, weapon:GetClass())
                 local ammotype = "none"
+                if weapon.Primary and weapon.Primary.Ammo then ammotype = weapon.Primary.Ammo end
 
-                if weapon.Primary and weapon.Primary.Ammo then
-                    ammotype = weapon.Primary.Ammo
-                end
-
-                if entity.dwr_shotThisTick == nil then
-                    entity.dwr_shotThisTick = false
-                end
-
+                if entity.dwr_shotThisTick == nil then entity.dwr_shotThisTick = false end
                 if entity.dwr_shotThisTick then return end
                 entity.dwr_shotThisTick = true
+                timer.Simple(engine.TickInterval()*2, function() entity.dwr_shotThisTick = false end)
 
-                timer.Simple(engine.TickInterval() * 2, function()
-                    entity.dwr_shotThisTick = false
-                end)
-
-                local data = {}
+                data = {}
                 data.Src = pos
                 data.Dir = dir
                 data.Vel = vel
-                data.Spread = Vector(0, 0, 0)
+                data.Spread = Vector(0,0,0)
                 data.Ammotype = ammotype
                 data.isSuppressed = isSuppressed
                 data.Entity = attacker
                 data.Weapon = attacker:GetActiveWeapon()
+
                 networkGunshotEvent(data)
             end)
         end)
@@ -309,6 +265,8 @@ end)
 
 hook.Add("EntityFireBullets", "dwr_EntityFireBullets", function(attacker, data)
     if data.Spread.z == 0.125 then return end -- for my blood decal workaround for mw sweps
+    if data.AmmoType == "grenadeFragments" then return end -- rfs support
+
     local entity = NULL
     local weapon = NULL
     local weaponIsWeird = false
@@ -321,57 +279,53 @@ hook.Add("EntityFireBullets", "dwr_EntityFireBullets", function(attacker, data)
     else
         weapon = attacker
         entity = weapon:GetOwner()
-
-        if entity == NULL then
+        if entity == NULL then 
             entity = attacker
             weaponIsWeird = true
         end
     end
 
-    -- should solve all of the issues caused by external bullet sources (such as the turret mod)
-    if not weaponIsWeird and weapon ~= NULL and entity.GetShootPos ~= nil then
+    if not weaponIsWeird and weapon != NULL and entity.GetShootPos != nil then -- should solve all of the issues caused by external bullet sources (such as the turret mod)
         local weaponClass = weapon:GetClass()
         local entityShootPos = entity:GetShootPos()
+
         if weaponClass == "mg_arrow" then return end -- mw2019 sweps crossbow
-        if weaponClass == "mg_sniper_bullet" and data.Spread == Vector(0, 0, 0) then return end -- physical bullets in mw2019
-        if weaponClass == "mg_slug" and data.Spread == Vector(0, 0, 0) then return end -- physical bullets in mw2019
+        if weaponClass == "mg_sniper_bullet" and data.Spread == Vector(0,0,0) then return end -- physical bullets in mw2019
+        if weaponClass == "mg_slug" and data.Spread == Vector(0,0,0) then return end -- physical bullets in mw2019
+
         if data.Distance < 200 then return end -- melee
 
         if string.StartWith(weaponClass, "arccw_") then
-            if data.Distance == 20000 then return end -- grenade launchers in arccw
-            if GetConVar("arccw_bullet_enable"):GetInt() == 1 and data.Spread == Vector(0, 0, 0) then return end -- bullet physics in arcw
+            if data.Distance == 20000 then -- grenade launchers in arccw
+                return
+            end
+            if GetConVar("arccw_bullet_enable"):GetInt() == 1 and data.Spread == Vector(0, 0, 0) then -- bullet physics in arcw
+                return
+            end
         end
 
         if string.StartWith(weaponClass, "arc9_") then
-            if GetConVar("arc9_bullet_physics"):GetInt() == 1 and data.Spread == Vector(0, 0, 0) then return end -- bullet physics in arc9
+            if GetConVar("arc9_bullet_physics"):GetInt() == 1 and data.Spread == Vector(0, 0, 0) then -- bullet physics in arc9
+                return
+            end
         end
 
-        if game.GetTimeScale() < 1 and data.Spread == Vector(0, 0, 0) and data.Tracer == 0 then return end -- FEAR bullet time
+        if game.GetTimeScale() < 1 and data.Spread == Vector(0,0,0) and data.Tracer == 0 then return end -- FEAR bullet time
 
-        if entity.dwr_shotThisTick == nil then
-            entity.dwr_shotThisTick = false
-        end
-
+        if entity.dwr_shotThisTick == nil then entity.dwr_shotThisTick = false end
         if entity.dwr_shotThisTick then return end
         entity.dwr_shotThisTick = true
-
-        timer.Simple(engine.TickInterval() * 2, function()
-            entity.dwr_shotThisTick = false
-        end)
-
-        if #data.AmmoType > 2 then
-            ammotype = data.AmmoType
-        elseif weapon.Primary then
-            ammotype = weapon.Primary.Ammo
-        end
-
+        timer.Simple(engine.TickInterval()*2, function() entity.dwr_shotThisTick = false end)
+                                                                                             
+    
+        if #data.AmmoType > 2 then ammotype = data.AmmoType elseif weapon.Primary then ammotype = weapon.Primary.Ammo end
         isSuppressed = getSuppressed(weapon, weaponClass)
     end
 
-    local dwr_data = {}
+    dwr_data = {}
     dwr_data.Src = data.Src
     dwr_data.Dir = data.Dir
-    dwr_data.Vel = Vector(0, 0, 0)
+    dwr_data.Vel = Vector(0,0,0)
     dwr_data.Spread = data.Spread
     dwr_data.Ammotype = ammotype
     dwr_data.isSuppressed = isSuppressed
@@ -385,17 +339,15 @@ hook.Add("EntityEmitSound", "dwr_EntityEmitSound", function(data)
     if not networkSoundsConvar:GetBool() then return end
     if not string.find(data.SoundName, "weapon") then return end
     if string.find(data.SoundName, "rpg") then return end
+
     local src = data.Entity:GetPos()
-
-    if data.Entity:IsPlayer() or data.Entity:IsNPC() then
-        src = data.Entity:GetShootPos()
-    end
-
+    if data.Entity:IsPlayer() or data.Entity:IsNPC() then src = data.Entity:GetShootPos() end
     data.Pos = src
-    net.Start("dwr_EntityEmitSound_networked")
-    net.WriteTable(data) -- send each element separately later
-    net.Broadcast()
-    data.Volume = 0
 
+    net.Start("dwr_EntityEmitSound_networked")
+        net.WriteTable(data) -- send each element separately later
+    net.Broadcast()
+
+    data.Volume = 0
     return true
 end)
